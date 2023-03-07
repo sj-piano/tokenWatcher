c02339073c43273ab589825d9b6362ca448a6286
StJohn Piano <stjohnpiano@gmail.com>
2023-03-07 09:18:50 +0000

    index.ts

---
+++ index.ts
@@ -0,0 +1,19 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Token Watcher\n');
+});
+
+app.get('/hello', (req: Request, res: Response) => {
+  res.send('Hello world\n');
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
